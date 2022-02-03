2472b1a4f56574f3f99ad282a35087d7f731e3e3
Nazariy <npogoriliy@eagles.ewu.edu>
2022-02-02 19:46:08 -0800

    Setting up some classes

---
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanHazFunny
+{
+    internal class Jester
+    {
+        public Jester()
+        {
+
+        }
+
+        public void TellJoke()
+        {
+
+        }
+    }
+}
