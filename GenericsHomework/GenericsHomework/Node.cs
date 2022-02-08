e20d8a14790c7c29aeaeb6e281c78aa735231af7
NazariyPogo <nazariypogo@gmail.com>
2022-02-07 20:07:35 -0800

    Made node class. Made tests for node class. Unit tested node class.

---
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -0,0 +1,61 @@
+﻿namespace GenericsHomework
+{
+    public class Node<T> where T : notnull
+    {
+        public Node(T value)
+        {
+            Value = value;
+            Next = this;
+        }
+
+        public T Value { get; private set; }
+        public Node<T> Next { get; private set; }
+
+        public void Append(T value)
+        {
+            if (Exists(value))
+            {
+                throw new ArgumentException("This value already exists");
+            }
+
+            Node<T> node = new Node<T>(value);
+            Node<T> tracer = this;
+
+           while(tracer.Next != this) { tracer = tracer.Next; }
+
+            tracer.Next = node;
+            node.Next = this;
+        }
+
+        public void Clear()
+        {
+            this.Next = this;
+        }   //Not sure what is meant by 'current node' in instructions
+
+        public bool Exists(T value)
+        {
+            if (this.Value.Equals(value)) { return true; }
+
+            Node<T> tracer = this;
+
+            for (; tracer.Next != this; tracer = tracer.Next)
+            {
+                if(tracer.Value.Equals(value)) { return true; }
+            }
+
+            if (tracer.Value.Equals(value)) { return true; }
+
+            return false;
+        }
+
+        public override string? ToString()
+        {
+            if(this.Value is null)
+            {
+                return null;
+            }
+
+            return this.Value.ToString();
+        }
+    }
+}
---
+++ GenericsHomework/GenericsHomeworkTests/NodeTests.cs
@@ -0,0 +1,92 @@
+using GenericsHomework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GenericsHomeworkTests
+{
+    [TestClass]
+    public class NodeTests
+    {
+        [TestMethod]
+        public void Node_ConstructorWorks()
+        {
+            Node<string> node = GetNode();
+            Assert.IsNotNull(node);
+        }
+
+        [TestMethod]
+        public void Node_ToString_Overridden()
+        {
+            Node<string> node = GetNode();
+            Assert.AreEqual("Inigo Montoya", node.ToString());
+        }
+
+        [TestMethod]
+        public void Node_ListLoopsOnItself()
+        {
+            Node<string> node = GetNode();
+            Assert.AreEqual(node, node.Next);
+        }
+
+        [TestMethod]
+        public void Node_Append_OriginalValueStays()
+        {
+            Node<string> node = GetNode();
+            node.Append("Princess Buttercup");
+            Assert.AreEqual(node.Value, "Inigo Montoya");
+        }
+
+        [TestMethod]
+        public void Node_Append_NewValueAppended()
+        {
+            Node<string> node = GetNode();
+            node.Append("Princess Buttercup");
+            Assert.AreEqual(node.Next.Value, "Princess Buttercup");
+        }
+
+        [TestMethod]
+        public void Node_ClearMethodWorks()
+        {
+            Node<string> node = GetNode();
+            node.Append("Princess Buttercup");
+            node.Clear();
+            Assert.AreEqual("Inigo Montoya", node.Next.Value);
+        }
+
+        [TestMethod]
+        public void Node_AppendArgumentException_FirstIF_ThrowsException()
+        {
+            Node<string> node = GetNode();
+
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                node.Append("Inigo Montoya");
+            });
+        }
+
+        [TestMethod]
+        public void Node_AppendArgumentException_SecondIF_ThrowsException()
+        {
+            Node<string> node = GetNode();
+            node.Append("Princess Buttercup");
+
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                node.Append("Princess Buttercup");
+            });
+        }
+
+        [TestMethod]
+        public void Node_ToStringReturnsNull()
+        {
+            string nullTest = null!;            //Used '!' to test null check in ToString
+            Node<string> node = new(nullTest)!; //^
+            Assert.IsNull(node.ToString());
+        }
+
+        public Node<string> GetNode()
+        {
+            return new Node<string>("Inigo Montoya");
+        }
+    }
+}
