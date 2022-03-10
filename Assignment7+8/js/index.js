aa114f1e296aa9409599d102422305f43cd540a9
NazariyPogo <nazariypogo@gmail.com>
2022-03-10 13:15:57 -0800

    Updated to partners code

---
+++ Assignment7+8/js/index.js
@@ -0,0 +1,40 @@
+function DropdownFunction() {
+  document.getElementById("dropdown").classList.toggle("show");
+}
+
+window.onclick = function(e) {
+if (!e.target.matches('.dropbtn')) {
+var dropdown = document.getElementById("dropdown");
+  if (dropdown.classList.contains('show')) {
+    dropdown.classList.remove('show');
+  }
+}
+}
+
+function apiPage(){
+window.location = 'https://v2.jokeapi.dev/joke/Programming';
+}
+
+let setup = document.querySelector(".joke");
+let delivery = document.querySelector(".delivery");
+
+function getNewJoke() {
+axios({
+  method: 'get',
+  url: 'https://v2.jokeapi.dev/joke/Programming?type=twopart'
+})
+  .then(function (response) {
+    delivery.innerText = ""
+    setup.innerText = "";
+    setup.innerText = response.data.setup;
+    setTimeout(function (){
+      delivery.innerText = response.data.delivery;
+    }, 4000, response)
+    })
+  .catch(function (error){
+    setup.innerText = "Error, please try again in a few moments!";
+    console.log(error);
+  });
+}
+
+window.onload = getNewJoke()
---
+++ MultithreadingStuff/Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+static void Main(string[] args)
+{
+    Console.WriteLine("Hello, World!");
+
+    Task.Run(
+        () => Display(",")
+        );
+
+    Console.WriteLine("ENTER to exit");
+    Console.ReadLine();
+}
+
+static void Display(char character)
+{
+    while (true)
+    {
+        Console.Write(character);
+    }
+}
